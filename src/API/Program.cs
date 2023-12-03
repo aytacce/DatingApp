c8400539c6c2da528c3b2a3f7bf732d1b80b475c
jakub-zulewski <j.zulewski@outlook.com>
2023-12-03 21:45:45 +0100

    Add communication between API and client.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -6,11 +6,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options
-    => options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
+	=> options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors(builder
+	=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.RegisterUsersEndpoints();
 
--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating app!';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('OK!'),
+    });
+  }
 }
